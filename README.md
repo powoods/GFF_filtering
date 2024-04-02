979b445b03a756971e78cf19495f4f90a5463afd
Patrick Woods <patrickodwoods@gmail.com>
2024-04-02 10:06:37 -0600

    First commit, updating README file and adding version 1 of the python script.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GFF_filtering
 Simple scripts to filter GFF or GTF files for various features.
+
+This repo currently contains a python script that can be used to filter GFF files for specific genomic features such as 'gene' or 'mRNA'.
---
+++ features_from_gff.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tuesday April 2 10:00:09 2024
+
+@author: patrick.woods
+"""
+
+#v1  
+
+def features_from_gff(gff, features_column, desired_feature):
+    '''
+    
+
+    Parameters
+    ----------
+    gff : String
+        The file name corresponding to the gff file to be filtered.
+    features_column : Integer
+        The column containing the feature values to filter by.
+    desired_feature : String
+        A string containing the name of the genetic feature to be filtered for such as 'gene' or 'mRNA'.
+
+    Returns
+    -------
+    file_sub : A pandas DataFrame type object
+        A pandas DataFrame type object containing only rows with the specified desired_feature string.
+
+    '''
+    import pandas as pd
+    import numpy as np
+    
+    file = pd.read_table(gff, header= None)
+    filt1 = file[features_column] == desired_feature
+    file_sub = file[filt1]
+    return file_sub
+
+#testing out the function
+features_from_gff('transcripts.fasta.transdecoder.genomeCentric.gff3', features_column = 2, desired_feature = 'gene')
