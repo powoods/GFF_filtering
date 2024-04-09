ea315a9f38412dbb6f1af54d6a4096c6f7d3c6cc
Patrick Woods <patrickodwoods@gmail.com>
2024-04-09 14:40:16 -0600

    Added the option to subset gff for features and contigs.

--- features_from_gff.py
+++ features_from_gff.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tuesday April 2 10:00:09 2024
+Created on Tuesday April 9 11:43:02 2024
 
 @author: patrick.woods
 """
 
-#v1  
+#v2
 
-def features_from_gff(gff, features_column, desired_feature):
+def features_from_gff(gff, features_column, desired_feature, contig_column=0, desired_contig='all'):
     '''
     
 
@@ -20,6 +20,10 @@ def features_from_gff(gff, features_column, desired_feature):
         The column index containing the feature values to extrct.
     desired_feature : String
         A string containing the name of the genetic feature to be extracted from the specified column index such as 'gene' or 'mRNA'.
+    contig_column : Integer
+        The column index containing the contig values to extract. Defaults to 0.
+    desired_contig : String
+        Defaults to 'all'. If a contig is specified, the function will return a dataframe of specified features from the specified contig.
 
     Returns
     -------
@@ -31,9 +35,27 @@ def features_from_gff(gff, features_column, desired_feature):
     import numpy as np
     
     file = pd.read_table(gff, header= None)
-    filt1 = file[features_column] == desired_feature
-    file_sub = file[filt1]
-    return file_sub
+    
+    feature_filt = file[features_column] == desired_feature
+    file_sub_feat = file[feature_filt]
+    
+    if desired_contig == 'all':
+        return file_sub_feat
+        
+    else:
+        contig_filt = file_sub_feat[contig_column] == desired_contig
+        file_sub_contig = file_sub_feat[contig_filt]
+    
+        return file_sub_contig
 
-#testing out the function
-features_from_gff('transcripts.fasta.transdecoder.genomeCentric.gff3', features_column = 2, desired_feature = 'gene')
+
+# Testing out function
+features_from_gff('../transcripts.fasta.transdecoder.genomeCentric.gff3', features_column = 2, desired_feature = 'gene', desired_contig='Scaffold_1533')
+
+
+
+
+
+
+
+
